aeff084b85766d5fbd2625eab6114814f922c6c1
Jason Zhang <jason721b@gmail.com>
2014-08-23 22:09:22 -0700

    Add support to Merriam-Webster

--- SearchDict.py
+++ SearchDict.py
@@ -1,12 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from bs4 import BeautifulSoup
 import requests, wget, sys, os
 
+MW_KEY = "59a2199b-d3f1-4eda-8c3f-8b1f570a4811"
+
 def main():
-  downloadAudio("university", "youdao", "en")
+  downloadAudio("los+angeles", "mw", "en")
+
 
 def downloadAudio(keyword, engine, path = None):
+  # data processing
+  keyword = unicode(keyword).strip().replace(" ","+")
   engine = unicode(engine).lower()
+  print 'Try to search "%s" using engine %s' % (keyword, engine)
+  
   found, url = audioLink(keyword, engine)
+  print "Return Audio Link: %s" % url
 
   if found == True:
     oldname = wget.download(url) 
@@ -16,16 +27,58 @@ def downloadAudio(keyword, engine, path = None):
   
 # return (true/false, audio_link)
 def audioLink(keyword, engine, **argvs):
-  if engine.lower() in ["youdao","youda", "yd"]:
-    type = argvs.get("type") or "1" # default = 1 if not specifiy 
-    query = u"http://dict.youdao.com/dictvoice?audio=%s&type=%s"
-    return True, query % (keyword, type)
+  engine = engine.lower()
+  
+  # Youdao 
+  if engine in ["youdao","youda", "yd"]:
+    # default = 1 if not specifiy
+    type = argvs.get("type") or "1" 
+    url = u"http://dict.youdao.com/dictvoice?audio=%s&type=%s" % (keyword, type)
+    return (True, url)
+ 
+  # Merriam-Webster
+  elif engine in ["merriam", "mw"]:
+    query = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/%s?key=%s"
+    print "Return Query URL: %s" % (query % (keyword, MW_KEY))
+    soup = getSoup(query % (keyword, MW_KEY))
+    url = parseMerriamWebsterSoup(soup)
+    if url != None:
+      return (True, url)
+    else:
+      return (False, None)
+  
+  # Invalid Engine Name
   else:
-    return False, None
+    return (False, None)
+
+
 
 ###################################
-######### Helper Function #########
+### Merriam-Webster 
 ###################################
+
+def parseMerriamWebsterSoup(soup):
+
+  if soup.find("suggestion") != None:
+    print "Found no result\nSuggested Words\n"
+    for (index, suggestion) in enumerate(soup.find_all("suggestion")):
+      print "%02i. %s\n" % ((index+1), suggestion.string) 
+    return None
+    
+  elif soup.find("sound") == None:
+    print "No audio file has been found."
+    return None
+  
+  else:
+    audioName = soup.find("sound").find("wav").string
+    url = "http://media.merriam-webster.com/soundc11/%s/%s" % (audioName[0], audioName)
+    return url    
+
+
+###################################
+### General Helper Function
+###################################
+
 
 def getSoup(url):
   r = requests.get(url)
